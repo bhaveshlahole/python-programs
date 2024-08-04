c80972f72d501e4a5cecb77bd8dd6c04bfe24076
bhaveshlahole <149697798+bhaveshlahole@users.noreply.github.com>
2024-08-04 14:40:35 +0530

    Add files via upload

---
+++ DictAndSets.py
@@ -0,0 +1,68 @@
+                    # DICTIONARY IN PYTHON
+"""
+inf = {
+     "name" : "Bhavesh",
+     "age"  : 21,
+     "marks" :[38,42,45,68,90],
+     "subject" : {
+         "ph":98,
+         "ch":97,
+         "mh":98,
+         "BL":100,
+         },
+     "total" : 100
+     }
+#print(inf)
+print(inf["subject"])
+print(inf["subject"]["ph"])
+print(inf.keys())
+print(inf.values())
+print(inf.items())
+
+inf.update({"city" : "Amravati"})
+print(inf)
+
+"""
+              # dictionary
+"""pract = {
+    "table" : ["a piece of furniture","A content of information"],
+    "cat" : "a small animal"
+
+    }
+print(pract)
+"""
+marks = {}
+x = int(input("Enter marks of physics"))
+marks.update({"phy":x})
+y = int(input("Enter marks of chemestry"))
+marks.update({"chem":y})
+z = int(input("Enter marks of maths"))
+marks.update({"maths":z})
+print(marks)
+
+
+
+                        # SETS IN PYTHON
+"""
+sets1 = {1,2,3,"Bhavesh","Nandkumar","Lahole"}
+sets2 = {3,4,5,6}
+sets1.add(4)
+sets1.add((5,6,7,8,9))
+sets1.remove(1)
+#sets.clear()
+print(sets1)
+print(sets1.union(sets2))
+
+"""
+"""
+cols1 = {1,2,3,4,5}
+cols2 = {4,5,6,7,8}
+print(cols1.union(cols2))
+print(cols1.intersection(cols2))
+"""
+          #
+"""collection ={"python","java","javascript","python","c++","java","python","javascript","c","java"}
+print(len(collection))
+"""
+
+                        
