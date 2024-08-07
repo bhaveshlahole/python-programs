9781dceb0ff841a80ff264e5d9c4f1246e7bea0d
bhaveshlahole <149697798+bhaveshlahole@users.noreply.github.com>
2024-08-07 22:29:12 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,80 @@
+"""name =input("enter your name:")
+print(name)
+
+a=int(input("enter value a:"))
+b=int(input("enter value b:"))
+
+print("the sum of two number is:",a+b)"""
+
+
+
+
+
+"""a=int(input("Enter your age:"))
+if(a>=18):
+    print("you are valid for vote")
+
+elif(a<=18):
+    print("you are not valid for vote")
+
+else:
+
+    print("Submit the foarm")"""
+
+
+
+
+
+"""light = input("Enter the color of light:")
+
+if(light == "red"):
+    print("Stop behind ")
+
+elif(light == "yellow"):
+    print("Be alert")
+
+elif(light == "green"):
+    print("Go")
+
+else:
+        print("signal is not working")"""
+
+
+
+"""marks = int(input("enter the marks out of 100 here:"))
+            
+if(marks >= 90):
+    print("A Grade")
+
+elif(marks >= 80 and marks < 90):
+    print("B Grade")
+
+elif(marks >= 70 and marks < 80):
+    print("C Grade")
+
+elif(marks >= 60 and marks < 70):
+    print("D Grade")
+
+else:
+    print("you have to study more")"""
+
+
+
+# CONDITIONAL STATEMENT(TERNARY OPERATOR)
+"""
+food = input("food:")
+eat = "yes" if food == "cake" else "no"
+print(eat)
+"""
+x = comp(input("Enter year"))
+print(x)
+
+
+
+
+
+
+    
+    
+
+
